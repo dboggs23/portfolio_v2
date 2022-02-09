03aa217180914a50171b5bbfa21f2d05686f1c27
Dalton Boggs <dpb522@mail.missouri.edu>
2022-02-09 16:01:09 -0600

    updated queries

--- web/src/templates/blog-post.js
+++ web/src/templates/blog-post.js
@@ -16,10 +16,6 @@ export const query = graphql`
         _id
         title
       }
-      mainImage {
-        ...SanityImage
-        alt
-      }
       title
       slug {
         current
@@ -29,27 +25,6 @@ export const query = graphql`
       authors {
         _key
         author {
-          image {
-            crop {
-              _key
-              _type
-              top
-              bottom
-              left
-              right
-            }
-            hotspot {
-              _key
-              _type
-              x
-              y
-              height
-              width
-            }
-            asset {
-              _id
-            }
-          }
           name
         }
       }
@@ -67,7 +42,6 @@ const BlogPostTemplate = (props) => {
         <SEO
           title={post.title || "Untitled"}
           description={toPlainText(post._rawExcerpt)}
-          image={post.mainImage}
         />
       )}
 

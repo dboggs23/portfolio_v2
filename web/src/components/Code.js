b4487b02d436cf2ab0e9700bf71b1d108b71bd3b
Dalton Boggs <dpb522@mail.missouri.edu>
2022-03-04 22:11:56 -0600

    added code blocks

---
+++ web/src/components/Code.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+export default ({ node }) => {
+  if (!node || !node.code) {
+    return null;
+  }
+  const { language, code } = node;
+  return (
+    <SyntaxHighlighter style={docco} language={language || "text"}>
+      {code}
+    </SyntaxHighlighter>
+  );
+};

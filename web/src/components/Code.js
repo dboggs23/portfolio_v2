0d4125fca99440bfbc809b662aed04c675aa580d
Dalton Boggs <dpb522@mail.missouri.edu>
2022-03-05 13:54:05 -0600

    dunno

--- web/src/components/Code.js
+++ web/src/components/Code.js
@@ -1,6 +1,4 @@
-/*import React from "react";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import React from "react";
 
 export default ({ node }) => {
   if (!node || !node.code) {
@@ -8,8 +6,9 @@ export default ({ node }) => {
   }
   const { language, code } = node;
   return (
-    <SyntaxHighlighter style={docco} language={language || "text"}>
-      {code}
-    </SyntaxHighlighter>
+    <div>
+      language = {language}
+      <pre>{code}</pre>
+    </div>
   );
-};*/
+};
